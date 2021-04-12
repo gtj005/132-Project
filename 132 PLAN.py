70f901e5345a2419ab38ae4a7bddedcff414268a
Gabe Jose <80844652+gtj005@users.noreply.github.com>
2021-04-12 12:01:30 -0500

    Add files via upload

--- 132 PLAN.py
+++ 132 PLAN.py
@@ -1,8 +1,24 @@
 ###Planning###
-#1.
-#2.
-#3.
-#4.
+
+#TODO TUESDAY
+#1.Create GUI
+#2.Create Process
+#3.Map out on paper/create maps
+#4.Map out on paper/create items
+#5.Inventory Processes
+#6.Think through ENEMY logic and stats (may finish thursday)
+#7.Attempt to define combat function (may finish thursday)
+#8.Research and Implement separate popup screens
+#9.Figure out time functions in python
+#10.Possibly run to Lowe's if not already done
+#11.
+#12.
+
+#TODO THURSDAY
+#1.Continue working
+#2.Finish what isn't complete
+#3.Implement researched topics
+#4.Possibly run to Lowe's if not already done
 #5.
 #6.
 #7.
@@ -11,3 +27,15 @@
 #10.
 #11.
 #12.
+
+#TODO
+#Build enclosure
+#Construct program
+#Research unknowns (Tkinter, time, lasercut, etc)
+#TEST, TEST, TEST
+#Make spotify work playlist
+#Meeting locations/times?
+    #Iesb study rooms?
+    #tolliver?
+    #graham basement?
+#
