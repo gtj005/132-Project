caa18d06e328a4c4e2258bc74c546263cfacf2bd
jake6857 <61104155+jake6857@users.noreply.github.com>
2021-04-13 17:01:01 -0500

    Add files via upload
    added many functions

--- Project.py
+++ Project.py
@@ -1,6 +1,5 @@
 from tkinter import *
 
-#create class for an enemy
 class Enemy():
     def __init__(self,name,HP,atkVal):
         self.name = name
@@ -30,8 +29,14 @@ class Area():
     def addItem(self, item):                      #adds items to the rooms
         self.items.append(item)
 
+    def delItem(self, item):
+        self.items.remove(item)
+
     def addEnemy(self, enemy):
         self.enemies.append(enemy)
+
+    def delEnemy(self, enemy):
+        self.enemies.remove(enemy)
     
 
 class Game(Frame):
@@ -39,11 +44,38 @@ class Game(Frame):
         Frame.__init__(self, parent)
 
     def createArea(self):
-        a1 = Area("Area 1", "pics/test.gif")
+        a1 = Area("test", "pics/test.gif")
 
         a1.addItem(Item("sword", 23))
-##        a1.addEnemy(Enemy("Jared", 3))
-            
+        a1.addEnemy(Enemy("Jared", 3, 3))
+
+
+
+
+        Game.currentArea = a1
+
+    def setupGUI(self):
+        self.pack(fill = BOTH, expand = 1)
+
+
+
+        img = None
+        Game.image = Label(self, width = WIDTH, image = img)
+        Game.image.pack(side = LEFT, fill = Y)
+        Game.image.pack_propagate(False)
+
+    def setAreaImage(self):
+        Game.img = PhotoImage(file = Game.currentArea.image)
+        Game.image.config(image = Game.img)
+        Game.image.image = Game.img
+
+
+
+    def play(self):
+        self.createArea()
+        self.setupGUI()
+        self.setAreaImage()
+        
 
 
 
@@ -75,11 +107,13 @@ class Game(Frame):
 
 
 
-
+HEIGHT = 800
+WIDTH = 800
 
 window = Tk()
-window.title("Avalanche's Fighters of the Forum")
+window.title("Avalache's Fighters of the forum")
 
 g = Game(window)
+g.play()
 
 window.mainloop()
